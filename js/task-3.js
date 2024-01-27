7e4f92100a8abb426c624f8e19bcb8a4701c65d0
illiakravchenko <illia.kravchenko@gmx.ch>
2024-01-27 18:09:00 +0100

    js дз1 2

--- js/task-3.js
+++ js/task-3.js
@@ -8,6 +8,9 @@ function getElementWidth(content, padding, border) {
     return totalWidth;
 }
 
+getElementWidth("50px", "8px", "4px");
+getElementWidth("60px", "12px", "8.5px");
+getElementWidth("200px", "0px", "0px");
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
